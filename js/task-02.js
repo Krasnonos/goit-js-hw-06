0e1f55bfe7a85714d201e91c44c1ee389d6da562
Krasnonos <andreykrasnonos@icloud.com>
2022-04-19 21:06:34 +0300

    update task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const itemsRef = ingredients.map(ingredient => {
+  const itemRef = document.createElement('li');
+  itemRef.classList.add('item');
+  itemRef.textContent = ingredient;
+  return itemRef;
+})
+
+ingredientsRef.append(...itemsRef);
