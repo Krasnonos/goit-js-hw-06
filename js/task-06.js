786474599f0942a1c8e2dd3cd1e37607c0c9453b
Krasnonos <andreykrasnonos@icloud.com>
2022-04-20 19:51:03 +0300

    fix task 6

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const inputRef = document.querySelector('#validation-input');
 inputRef.addEventListener('blur', validationForm);
 
 function validationForm(event) {
-    if (event.currentTarget.value.length < inputRef.dataset.length) {
+    if (event.currentTarget.value.length === inputRef.dataset.length) {
         inputRef.classList.add('invalid')
         inputRef.classList.remove('valid')
     } else {
